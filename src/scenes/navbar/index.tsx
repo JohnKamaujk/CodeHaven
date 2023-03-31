b8aabd2b61188693c6878eac79bac637d60e4f31
JohnKamaujk <johnjkkamau@gmail.com>
2023-03-31 13:01:01 +0300

    :sparkles:feat:Imported react anchor link for smooth scrolling

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "@/scenes/navbar/Link";
 
-type Props = {};
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage,setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
     <nav>
@@ -17,10 +21,10 @@ const Navbar = (props: Props) => {
             {/* {right side} */}
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>classes</p>
-                <p>About</p>
+                <Link page="Home" selectedPage={selectedPage} setSelectedPage ={setSelectedPage}/>
+                <Link page="Benefits" />
+                <Link page="Our Classes" />
+                <Link page="Contact Us" />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign in</p>
