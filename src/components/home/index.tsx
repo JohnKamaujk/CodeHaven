3a17237c2de6855562870cbdee832e7952f2c095
JohnKamaujk <johnjkkamau@gmail.com>
2023-04-02 13:12:11 +0300

    :sparkles:feat:added layout to hero section

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -18,7 +18,50 @@ const Home = ({ setSelectedPage }: Props) => {
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      Home
+      {/* IMAGE AND MAIN HEADER */}
+      <motion.div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <motion.div>
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+          {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+        {/* IMAGE */}
+        <div></div>
+      </motion.div>
     </section>
   );
 };
