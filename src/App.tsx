9f767bb4f4e974b45557e8f5ed4fd1a3cbad572d
JohnKamaujk <johnjkkamau@gmail.com>
2023-04-10 19:56:20 +0300

    :sparkles:feat:contact us created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar";
 import Home from "@/components/home";
 import Benefits from "@/components/benefits";
 import OurClasses from "@/components/ourClasses";
+import ContactUs from "@/components/contactUs";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -33,6 +34,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs />
     </div>
   );
 }
